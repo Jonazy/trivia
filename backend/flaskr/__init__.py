119cd8068153627a42abdf0ce96d2c8330a44bc0
jonazy <jonas.humenu@gmail.com>
2022-07-13 08:30:31 +0100

    Bug fix

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -46,175 +46,169 @@ def create_app(test_config=None):
       'result': categories,
     }), HTTP_200_OK
 
-@app.route('/questions', methods=['GET'])
-def questions():
-  # pagination
-  page = request.args.get('page', 1, type=int)
-
-  questions = Question.query.peginate(page=page, per_page=10)
-  count = len(Question.query.all())
-
-  current_categories = []
-  for c_categories in questions.caetgory:
-    current_categories.append(c_categories)
-
-  categories = []
-  for category in Category.query.all():
-    categories.append(category.format())
-
-  return jsonify({
-    "success": True,
-    "count": count,
-    "questions": questions,
-    "current_category": current_categories,
-    "categories": categories
-  }), HTTP_200_OK
-
-@app.route('/question/<int:id>', methods=['DELETE'])
-def question(id):
-  try:
-    question = Question.query.filter_by(id=id).first()
-    if question is None:
-      return jsonify({
-        "success": False,
-        "message": "Record not found"
-      }), HTTP_404_NOT_FOUND
-    else:
-      return jsonify({
-        "success": True
-      }), HTTP_200_OK
-  except:
-    return jsonify({
-      "success": False,
-      "messsage": "something went wrong"
-    }), HTTP_500_INTERNAL_SERVER_ERROR
-
-@app.route('/question', methods=['POST'])
-def question():
-  question = request.get_json().get('question', None)
-  answer = request.get_json().get('answer', None)
-  difficulty = request.get_json().get('difficulty', None)
-
-  if question is "":
-    return jsonify({
-      "success": False,
-      "message": "Question is required!"
-    }), HTTP_400_BAD_REQUEST
-
-  if answer is "":
-    return jsonify({
-      "success": False,
-      "message": "Answer is required!"
-    }), HTTP_400_BAD_REQUEST
-
-  if category is "":
-    return jsonify({
-      "success": False,
-      "message": "Category is required!"
-    }), HTTP_400_BAD_REQUEST
-
-  if difficulty is "":
-    return jsonify({
-      "success": False,
-      "message": "Difficulty is required!"
-    }), HTTP_400_BAD_REQUEST
-
-  try:
-    question = Question(question=question, answer=answer, category=category, difficulty=difficulty)
-    question.insert()
-    count = len(Question.query.all())
+  @app.route('/questions', methods=['GET'])
+  def questions():
+    # pagination
     page = request.args.get('page', 1, type=int)
-    questions = Question.query.paginate(page=page, per_page=10)
+
+    questions = Question.query.peginate(page=page, per_page=QUESTIONS_PER_PAGE)
+    count = len(Question.query.all())
+
+    current_categories = []
+    for c_categories in questions.caetgory:
+      current_categories.append(c_categories)
+
+    categories = []
+    for category in Category.query.all():
+      categories.append(category.format())
 
     return jsonify({
       "success": True,
+      "count": count,
       "questions": questions,
-      "count": count
-    }), HTTP_201_CREATED
+      "current_category": current_categories,
+      "categories": categories
+    }), HTTP_200_OK
 
-  except:
-    return jsonify({
-      "success": False,
-      "message": "something went wrong"
-    }), HTTP_500_INTERNAL_SERVER_ERROR
-
-@app.route('/questions', methods=['POST'])
-def question_search():
-  search_string = request.get_json().get('search_key', None)
-  if search_string is "":
-    return jsonify({
-      "success": False,
-      "message": "Enter search word!"
-    }), HTTP_400_BAD_REQUEST
-
-  try:
-    search = Question.query.filter(Question.query.question.contains(search_string)).order_by(Question.id).all()
-    if search is not None:
-      return jsonify({
-          "success": True,
-          "questions": search
-        }), HTTP_200_OK
-    else:
-      return jsonify({
-          "success": False,
-          "questions": search
-        }), HTTP_404_NOT_FOUND
-
-  except:
-    return jsonify({
-      "success": False,
-      "message": "something went wrong"
-    }), HTTP_500_INTERNAL_SERVER_ERROR
-
-@app.route('/questions/<str:category>', methods=['GET'])
-def questions_by_category(category):
-  questions = Question.query.filter_by(category=category).ordery_by(Question.id).all()
-
-  if questions is None:
-    return jsonify({
+  @app.route('/question/<int:id>', methods=['DELETE'])
+  def question(id):
+    try:
+      question = Question.query.filter_by(id=id).first()
+      if question is None:
+        return jsonify({
           "success": False,
           "message": "Record not found"
         }), HTTP_404_NOT_FOUND
-
-  else:
-    return jsonify({
-          "success": True,
-          "questions": questions
+      else:
+        return jsonify({
+          "success": True
         }), HTTP_200_OK
+    except:
+      return jsonify({
+        "success": False,
+        "messsage": "something went wrong"
+      }), HTTP_500_INTERNAL_SERVER_ERROR
 
-@app.route('/quizzes', methods=['POST'])
-def quizzes():
-  category = request.get_json.get('quiz_category', None)
-  previous_questions = request.get_json.get('previous_questions', None)
+  @app.route('/question', methods=['POST'])
+  def question():
+    question = request.get_json().get('question', None)
+    answer = request.get_json().get('answer', None)
+    difficulty = request.get_json().get('difficulty', None)
 
-  if category is "":
+    if question is "":
+      return jsonify({
+        "success": False,
+        "message": "Question is required!"
+      }), HTTP_400_BAD_REQUEST
+
+    if answer is "":
+      return jsonify({
+        "success": False,
+        "message": "Answer is required!"
+      }), HTTP_400_BAD_REQUEST
+
+    if category is "":
+      return jsonify({
+        "success": False,
+        "message": "Category is required!"
+      }), HTTP_400_BAD_REQUEST
+
+    if difficulty is "":
+      return jsonify({
+        "success": False,
+        "message": "Difficulty is required!"
+      }), HTTP_400_BAD_REQUEST
+
+    try:
+      question = Question(question=question, answer=answer, category=category, difficulty=difficulty)
+      question.insert()
+      count = len(Question.query.all())
+      page = request.args.get('page', 1, type=int)
+      questions = Question.query.paginate(page=page, per_page=QUESTIONS_PER_PAGE)
+
+      return jsonify({
+        "success": True,
+        "questions": questions,
+        "count": count
+      }), HTTP_201_CREATED
+
+    except:
+      return jsonify({
+        "success": False,
+        "message": "something went wrong"
+      }), HTTP_500_INTERNAL_SERVER_ERROR
+
+  @app.route('/questions', methods=['POST'])
+  def question_search():
+    search_string = request.get_json().get('search_key', None)
+    if search_string is "":
+      return jsonify({
+        "success": False,
+        "message": "Enter search word!"
+      }), HTTP_400_BAD_REQUEST
+
+    try:
+      search = Question.query.filter(Question.query.question.contains(search_string)).order_by(Question.id).all()
+      if search is not None:
+        return jsonify({
+            "success": True,
+            "questions": search
+          }), HTTP_200_OK
+      else:
+        return jsonify({
+            "success": False,
+            "questions": search
+          }), HTTP_404_NOT_FOUND
+
+    except:
+      return jsonify({
+        "success": False,
+        "message": "something went wrong"
+      }), HTTP_500_INTERNAL_SERVER_ERROR
+
+  @app.route('/questions/<str:category>', methods=['GET'])
+  def questions_by_category(category):
+    questions = Question.query.filter_by(category=category).ordery_by(Question.id).all()
+
+    if questions is None:
+      return jsonify({
+            "success": False,
+            "message": "Record not found"
+          }), HTTP_404_NOT_FOUND
+
+    else:
+      return jsonify({
+            "success": True,
+            "questions": questions
+          }), HTTP_200_OK
+
+  @app.route('/quizzes', methods=['POST'])
+  def quizzes():
+    category = request.get_json.get('quiz_category', None)
+    previous_questions = request.get_json.get('previous_questions', None)
+
+    if category is "":
+      return jsonify({
+        "success": False,
+        "message": "Category is required!"
+      }), HTTP_400_BAD_REQUEST
+    if previous_questions is None:
+      if category:
+        questions = Question.query.filter(Question.category==category).all()
+      else:
+        questions = Question.query.filter.all()
+    else:
+      if category:
+        questions = Question.query.filter(Question.id != previous_questions, Question.category==category).all()
+      else:
+        questions = Question.query.filter(Question.id != previous_questions).all()
+
+    random_question = random.randint(0, len(questions))
+
     return jsonify({
-      "success": False,
-      "message": "Category is required!"
-    }), HTTP_400_BAD_REQUEST
-  if previous_questions is None:
-    if category:
-      questions = Question.query.filter(Question.category==category).all()
-    else:
-      questions = Question.query.filter.all()
-  else:
-    if category:
-      questions = Question.query.filter(Question.id != previous_questions, Question.category==category).all()
-    else:
-      questions = Question.query.filter(Question.id != previous_questions).all()
-
-  random_question = random.randint(0, len(questions))
-
-  return jsonify({
-    "success": True,
-    "quizzes": random_question
-  }), HTTP_200_OK
-
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+      "success": True,
+      "quizzes": random_question
+    }), HTTP_200_OK
 
   @app.errorhandler(422)
   def unprocessable(error):
